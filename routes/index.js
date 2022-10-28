3ee0bc4eb98d5516c86443e59ad4c41020de4a9c
Caprtainx <benjamin.n.medina12@gmail.com>
2022-10-28 13:41:36 -0700

    all of jims steps

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,32 @@
 var express = require('express');
 var router = express.Router();
+const passport = require('passport');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Best Game Reviews' });
+});
+
+router.get('/auth/google', passport.authenticate(
+  'google',
+  {
+    scope: ['profile', 'email'],
+    prompt: "select_account"
+  }
+));
+
+router.get('/oath2callback', passport.authenticate(
+  'google',
+  {
+    successRedirect: '/games',
+    failureRedirect: '/games'
+  }
+));
+
+router.get('/logout', function(req, res) {
+  req.logout(function() {
+    res.redirect('/movies');
+  });
 });
 
 module.exports = router;
