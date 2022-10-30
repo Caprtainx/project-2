4ffd9342e683b0d5054505659da6b25931defb89
Caprtainx <benjamin.n.medina12@gmail.com>
2022-10-30 16:39:06 -0700

    oauth

--- models/game.js
+++ models/game.js
@@ -3,7 +3,24 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const gameSchema = new Schema({
-    title: String,
-    releaseDate: Date,
-    company: String,
+    title: {
+        type:String,
+    },
+    releaseDate: {
+        type: Date,
+    },
+    company: {
+        type: String,
+    },
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+      },
+      userName: String,
+      userAvatar: String
+    }, {
+      timestamps: true
 });
+
+module.exports = mongoose.model('Game', gameSchema);
