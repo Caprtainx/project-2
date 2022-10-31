f93f307110682a783087389146ae45233fc7d5ed
Caprtainx <benjamin.n.medina12@gmail.com>
2022-10-31 12:34:48 -0700

    create post

--- controllers/games.js
+++ controllers/games.js
@@ -2,11 +2,24 @@ const Game = require('../models/game');
 
 module.exports = {
     index,
-    new: newGame
+    new: newGame,
+    create
+}
+
+function create(req, res) {
+    req.body.user = req.user._id;
+    req.body.userName = req.user.name;
+    req.body.userAvatar = req.user.avatar;
+    const game = new Game(req.body);
+    game.save(function(err) {
+    if (err) return res.redirect('/games/new');
+    console.log(game);
+    res.redirect('/games');
+  });
 }
 
 function newGame(req, res) {
-    res.render('movies/new', { title: 'Add Movie' });
+    res.render('games/new', { title: 'Add Game' });
 }
 
 function index(req, res) {
--- routes/games.js
+++ routes/games.js
@@ -1,9 +1,11 @@
 var express = require('express');
+const games = require('../controllers/games');
 var router = express.Router();
 const gamesCtrl = require('../controllers/games');
 
 router.get('/', gamesCtrl.index)
 router.get('/new', gamesCtrl.new)
+router.post('/', gamesCtrl.create)
 
 
 module.exports = router;
