91726717fc8b7dee7ef93d92c9f0e727e3ae4838
Caprtainx <benjamin.n.medina12@gmail.com>
2022-11-02 10:06:02 -0700

    Deploy to heroku

--- routes/comments.js
+++ routes/comments.js
@@ -5,7 +5,7 @@ const ensureLoggedIn = require('../config/ensureLoggedIn');
 
 router.post('/games/:id/comments', ensureLoggedIn, commentsCtrl.create)
 router.delete('/comments/:id', ensureLoggedIn, commentsCtrl.delete);
-router.put('/comments/:id', commentsCtrl.update)
+router.put('/comments/:id', ensureLoggedIn, commentsCtrl.update)
 // router.get('/comments/:id/edit', ensureLoggedIn, commentsCtrl.edit)
 
 module.exports = router;
